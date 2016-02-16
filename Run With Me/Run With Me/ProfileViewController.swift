e8fe41f4100db70b54cd9c1c58ed06f09d7aabae
Edrick Pascual <edrickpascual@Edricks-MacBook-Pro.local>
2016-02-15 16:12:55 -0800

    Profile Picture Edit Button Added
    Profile Picture Edit Button Added

---
+++ Run With Me/Run With Me/ProfileViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ProfileViewController.swift
+//  Run With Me
+//
+//  Created by Edrick Pascual on 2/15/16.
+//  Copyright © 2016 Edrick Pascual. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var profilePicture: UIImageView!
+    
+    
+    
+    @IBAction func editPicture(sender: AnyObject) {
+        
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        image.allowsEditing = false
+        
+        self.presentViewController(image, animated: true, completion: nil)
+        
+    }
+    
+    // FUNCTION for the Image Picker Controller
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+
+        profilePicture.image = image
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
